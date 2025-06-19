aa6429d21427fc6322da3f5f21db9423e066f52a
ilya-korotya <officalbatya@gmail.com>
2025-06-19 20:33:59 +0200

    commit frotend part

--- POU-integration-for-verifiers/static/index.js
+++ POU-integration-for-verifiers/static/index.js
@@ -1,6 +1,42 @@
 // Set the base URL for the API request
 const baseUrl = `${window.location.origin}${window.location.pathname}`;
 
+// Function to poll status
+function pollStatus(requestId) {
+  const checkStatus = () => {
+    fetch(`${baseUrl}api/status/${requestId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Status:", data.status);
+        if (data.status !== "success") {
+          // Continue polling if not successful
+          setTimeout(checkStatus, 2000); // Poll every 2 seconds
+        } else {
+          console.log("Verification completed successfully!");
+          if (
+            confirm(
+              "✅ Verification completed successfully!\nWould you like to reload the page?"
+            )
+          ) {
+            window.location.reload();
+          }
+        }
+      })
+      .catch((error) => {
+        console.error("Error checking status:", error);
+        if (
+          confirm(
+            "❌ Error occurred during verification.\nWould you like to restart the page to try again?"
+          )
+        ) {
+          window.location.reload();
+        }
+      });
+  };
+
+  checkStatus();
+}
+
 // Function to handle the page load event
 window.onload = () => {
     const linkButton = document.getElementById('button');
@@ -17,7 +53,7 @@ window.onload = () => {
         .then(data => {
             // Encode the data in Base64 for the universal link
             const encodedRequest = btoa(JSON.stringify(data));
-            linkButton.href = `https://wallet.privado.id/#i_m=${encodedRequest}`;
+            linkButton.href = `https://wallet-dev.privado.id/#i_m=${encodedRequest}`;
             linkButton.style.display = 'block'; // Show the universal link button
         })
         .catch(error => console.error('Error fetching data from API:', error));
